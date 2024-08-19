511f88e27990d5548105bf49141e05ef06f95be4
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-08-19 12:42:44 +0700

    Hello World

---
+++ typescript01/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function something(msg) {
+    console.log(msg);
+    return 1;
+}
+something("Hello World");
---
+++ typescript01/index.ts
@@ -0,0 +1,5 @@
+function something(msg: string): number {
+    console.log(msg);
+    return 1;
+}
+something("Hello World");
