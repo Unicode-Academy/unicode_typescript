78e238b4df66dbafcf5f9d5cf4c0dc96cb913549
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-08-19 15:48:24 +0700

    Data Type: Kieu cau truc

---
+++ typescript01/src/ex04.ts
@@ -0,0 +1,23 @@
+//array: Xác định kiểu dữ liệu sẽ sử dụng trong mảng
+//Ví dụ: mảng number, mảng string,...
+// const numbers: number[] = [1, 2, 3];
+// const users: any[] = [];
+// users.push("Unicode", 1, true, false);
+// const users: object[] = [];
+// users.push({
+//   id: 1,
+//   name: "User 1",
+// });
+// users.push("Hoàng An");
+// const numbers: number[] = [];
+
+//tuple
+// const users: [string, number, boolean, object] = ["An", 10, false, {}];
+// console.log(users);
+
+//object
+// const user: { name: string; age: number; status: boolean } = {
+//   name: "An",
+//   age: 32,
+//   status: "true",
+// };
