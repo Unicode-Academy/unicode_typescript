7a3dc8ad9ebd9b2fe94680714d0e63c28becc52e
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-08-20 16:54:22 +0700

    Kieu du lieu tu dinh nghia

---
+++ typescript01/src/ex05.ts
@@ -0,0 +1,25 @@
+// enum Status {
+//   PENDING = "pending",
+//   PUBLISH = "publish",
+//   FAILED = "failed",
+// }
+// let updateStatus: Status = Status.PENDING;
+// console.log(updateStatus);
+
+// type Point = [number, number];
+
+// let point: Point = [10, "10"];
+
+// console.log(point);
+
+// interface Person {
+//   name: string;
+//   age: number;
+// }
+
+// const user: Person = {
+//   name: "An",
+//   age: "32",
+// };
+
+// console.log(user);
