3d5726b28f56361a5e512c30a13818ebb408e266
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-08-23 01:37:41 +0700

    Literal Types

---
+++ typescript01/src/ex08.ts
@@ -0,0 +1,29 @@
+//Literal Types
+
+// enum ShapeKind {
+//   Circle = "circle",
+//   Square = "square",
+// }
+
+// function getArea(shape: ShapeKind) {
+//   switch (shape) {
+//     case ShapeKind.Circle:
+//       return Math.PI * Math.pow(10, 2); // Giả định radius = 10;
+//     case ShapeKind.Square:
+//       return Math.pow(10, 2); // Giả định cạnh = 10
+//     default:
+//       return 0;
+//   }
+// }
+
+// type ShapeKind = "circle" | "square";
+// function getArea(shape: ShapeKind) {
+//   switch (shape) {
+//     case "circle":
+//       return Math.PI * Math.pow(10, 2); // Giả định radius = 10;
+//     case "square":
+//       return Math.pow(10, 2); // Giả định cạnh = 10
+//     default:
+//       return 0;
+//   }
+// }
