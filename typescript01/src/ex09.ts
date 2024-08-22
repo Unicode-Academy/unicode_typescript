e24cb94f33d8babd42d0335d8f78a74fd9799c35
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-08-23 01:51:56 +0700

    readonly keyword

---
+++ typescript01/src/ex09.ts
@@ -0,0 +1,29 @@
+// interface Point {
+//   readonly x: number;
+//   readonly y: number;
+// }
+
+// const point: Point = {
+//   x: 10,
+//   y: 20,
+// };
+// point.x = 20;
+
+// class Car {
+//   readonly make: string;
+//   readonly model: string;
+//   constructor(make: string, model: string) {
+//     this.make = make;
+//     this.model = model;
+//   }
+// }
+
+// const car = new Car("Honda", "Civic");
+// car.make = "Toyota";
+
+// let arr: readonly number[] = [1, 2, 3];
+// arr[0] = 10;
+// arr.push(4);
+
+// let arr: ReadonlyArray<string> = ["A", "B", "C"];
+// arr.push("D");
