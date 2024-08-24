30d36f5dc2a7808eac10186e01a9913ae4561865
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-08-24 15:42:13 +0700

    Function

---
+++ typescript02/build/ex01.js
@@ -0,0 +1,20 @@
+"use strict";
+//Function
+// - Kiểu trả về
+// - Kiểu của tham số
+// - Hàm với tham số mặc định
+// - Tham số tùy chọn
+// - Arrow Function (Lambda)
+// ==> Học ES6 JS
+// function sum(a: number, b: number, c?: number): void {
+//   //   return a + b;
+//   const total: number = a + b;
+//   console.log(total);
+// }
+// sum(10, 20);
+// const sum = (a: number, b: number): number => a + b;
+// sum(10, 20);
+// const sum = (a: number, b: number, c?: number): number => {
+//   return a + b;
+// };
+// console.log(sum(10, 20));
